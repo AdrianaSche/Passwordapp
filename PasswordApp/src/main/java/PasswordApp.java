d6b5924196ba4a0d613684e877e304acc4f15a94
Sebastian <sebastian@Sebastians-MBP.fritz.box>
2021-07-16 12:04:30 +0200

    check Length Funktion implementiert

--- PasswordApp/src/main/java/PasswordApp.java
+++ PasswordApp/src/main/java/PasswordApp.java
@@ -1,3 +1,32 @@
 public class PasswordApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean validatePassword(String password) {
+        if (password == null) {
+            System.out.println("Nullpointer-Fehler");
+            return false;
+
+        }
+        boolean lengthOK = checkLength(password);
+        return lengthOK;
+    }
+
+    public static boolean checkLength(String password) {
+        int passwordLength = password.length();
+        if (passwordLength < 7) {
+            System.out.println("Passwort zu kurz");
+            return false;
+        } else if (passwordLength > 20) {
+            System.out.println("Passwort ist zu lang");
+            return false;
+        }
+
+        return true;
+    }
+    public static boolean containsCapital(String password) {
+
+    }
 
 }
