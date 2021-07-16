f5fb6dd3ebc046130365c376f8c938ccfc20bb55
Alessandra Ipiranga <alessandraipiranga@MacBook-Pro.local>
2021-07-16 11:31:14 +0200

    Tests added

--- PasswordApp/src/main/java/PasswordApp.java
+++ PasswordApp/src/main/java/PasswordApp.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class PasswordApp {
+public class PasswordApp {
+
 }
--- PasswordApp/src/test/java/PasswordAppTest.java
+++ PasswordApp/src/test/java/PasswordAppTest.java
@@ -1,2 +1,95 @@
-package PACKAGE_NAME;public class PasswordAppTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordAppTest {
+
+    @Test
+    public void checkInputNull() {
+        //given
+        String password = null;
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkInputEmpty() {
+        //given
+        String password = "";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkInputToShort() {
+        //given
+        String password = "banane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkInputToLong() {
+        //given
+        String password = "bananebananebananebananebananebananebananebananebanane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkInputLengthIsValide() {
+        //given
+        String password = "bananebanane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkInputHasCapital() {
+        //given
+        String password = "bAnAnEbAnAnE";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkInputHasLower() {
+        //given
+        String password = "bananebanane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkInputHasNumber() {
+        //given
+        String password = "bananebanane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    public void checkInputBlank() {
+        //given
+        String password = "banane banane";
+        //when
+        boolean actual = PasswordApp.validatePassword(password);
+        //then
+        assertFalse(actual);
+    }
 }
